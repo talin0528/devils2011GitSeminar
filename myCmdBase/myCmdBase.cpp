d40f9fea16e473baf58ded293fc3434facde8b60
talin0528 <pan266@naver.com>
2011-07-01 03:05:01 +0900

    ksh commit

--- myCmdBase/myCmdBase.cpp
+++ myCmdBase/myCmdBase.cpp
@@ -10,6 +10,7 @@ using namespace std;
 #define ARG_NUM 20
 
 #include "cmdTest.h"
+#include "ksh_cmd.h"
 
 typedef char (*ARGV)[ARG_SIZE];
 typedef int (*PF)(int, ARGV);
@@ -18,7 +19,7 @@ struct Cmds{
 	char cmdName[256];
 	PF cmd;
 } cmds[] = {{"pf1", testCmd},
-			{"pf1", testCmd},  // 이곳에 함수를 추가하면 됩니다.
+			{"pf3", ksh_testCmd},  // 이곳에 함수를 추가하면 됩니다.
 			{"pf2", testCmd}};
 
 int parseCommand(char* str, ARGV argv);
@@ -54,8 +55,8 @@ int parseCommand(char* str, ARGV argv){
 
 // 가장 앞 문자열을 이용하여 일치하는 커맨드를 가져온다.
 PF getCommander(ARGV argv){
-	for( int i=0; i<sizeof(Cmds)/sizeof(cmds); i++)
+	for( int i=0; i<sizeof(cmds)/sizeof(Cmds); i++)
 		if( strcmp(argv[0], cmds[i].cmdName ) == 0 )
 			return cmds[i].cmd;
-	return NULL;
+	return testCmd;
 }
