a17c208a6c36eae10d978e7a59b540dd3ea97443
talin0528 <pan266@naver.com>
2011-07-01 03:06:42 +0900

    ksh commit!

---
+++ myCmdBase/ksh_cmd.cpp
@@ -0,0 +1,7 @@
+#include "ksh_cmd.h"
+int ksh_testCmd(int argc, char (*argv)[ARG_SIZE]){
+	for(int i=0;i<1000;i++)
+		cout<<"¾Æ";
+	cout<<"Ú¤Ð¤Ð"<<endl;
+	return 0;
+}
---
+++ myCmdBase/ksh_cmd.h
@@ -0,0 +1,5 @@
+#include <iostream>
+using namespace std;
+#define ARG_SIZE 256
+
+int ksh_testCmd(int argc, char (*argv)[ARG_SIZE]);
